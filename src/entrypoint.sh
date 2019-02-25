c3f42b8a0eac1088107d268c69c5dde6669da2c5
daniel.boucher <daniel.boucher@cauca.ca>
2019-02-25 14:42:10 -0500

    Enable ip_nonlocal_bind for IPv4/IPv6

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -16,6 +16,7 @@
 env | egrep -v '^PATH=' >> /etc/environment
 echo 0 | tee /proc/sys/net/ipv4/conf/*/rp_filter > /dev/null
 echo 1 | tee /proc/sys/net/ipv4/ip_forward > /dev/null
+echo 1 | tee /proc/sys/net/ipv?/ip_nonlocal_bind > /dev/null
 echo 1 | tee /proc/sys/net/ipv6/conf/*/forwarding > /dev/null
 rm -rf /run/*
 exec runit-docker
